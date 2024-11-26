f698bd75e2cf8c8e468fdfb3acd07b92e551b131
Someia <someia.ouaziz@gmail.com>
2024-11-26 16:35:39 +0100

    lastupdate

---
+++ script.js
@@ -0,0 +1,13 @@
+//rôle: entrer le choix de mot utilisateur
+//paramètre: NON
+//retour: NON
+
+//Créer la variable du choix du mot de l'Utisateur 
+
+function ChoixUser() {²
+    console.log(ChoixUser);
+    
+}
+    
+
+//je crée ma boucle pour récuperer chaque letttre
